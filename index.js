ab46d7fff30b88091f3dccf01cbae72e97b3db67
jaruwat420 <jaruwatcj@gmail.com>
2023-10-10 11:05:36 +0700

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const fs = require('fs');
 const http = require('http');
+const path = require('path');
 
-for (let i = 0; i < 100; i++) {
-    const element = i;
-    setTimeout(() => {
-        const request = http.request(`http://localhost:3000/test/${i}`);
-        request.end();
-    }, element);
 
-}
+const server = http.createServer((req, res) => {
+    const {method, url} = req;
+
+    try {
+        fs.statSync('logs');
+
+    } catch (error) {
+        fs.mkdir('logs');
+    }
+
+    const logContent = `${new Date()} : ${method} ${url}\n`;
+    fs.writeFileSync(path('logs', 'res.log'), logContent, { flag: 'a+'});
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    res.end(`<h1>Hello world</h1>`)
+
+});
+server.listen(3000, ()=>{
+    console.log(`server running on port 3000`);
+});
